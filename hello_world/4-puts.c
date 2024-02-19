8c72dad6c40add290461cc8f148d6fbd9bc7b7a6
LOICKITOKO <8761@holbertonstudents.com>
2024-02-19 15:41:19 +0100

    mm4

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 /**
-* main - Entry point
-* Return: Always 0 (Succecc)
-*/
+ *main - Entry point
+ *Return: Always 0 (Success)
+ */
 int main(void)
 {
-	puts("\"programming is like building a multilingual puzzle\n");
+	puts("\"programming is like building a multilingual puzzle");
 	return (0);
 }
