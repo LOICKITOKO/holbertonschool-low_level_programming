b5d5383094fb52b94c65f20a4cf5736ef676f4b9
LOICKITOKO <8761@holbertonstudents.com>
2024-03-15 11:14:12 +0100

    ash0

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -1,6 +1,15 @@
 #ifndef _DOG_H_
 #define _DOG_H_
 
+/**
+ * struct dog - a structure representing a dog
+ * @name: pointer to a character string representing the name of the dog
+ * @age: age of the dog
+ * @owner: pointer to a character string representing the owner of the dog
+ *
+ * Description: This structure represents a dog with its name, age, and owner.
+ */
+
 struct dog
 {
 	char *name;
